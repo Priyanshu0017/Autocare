755da049d4bd1bb749fa6f8dc1095a88622e4be3
Priyanshu0017 <priyanshubodana9@gmail.com>
2025-04-24 01:46:31 +0530

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api':{
-        target : 'https://lapcare01.onrender.com',
+        target : 'http://localhost:6000',
         changeOrigin: true,
         secure:false
       }
